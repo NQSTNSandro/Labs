5eeeffcb8b26eae17f3b957e09050dda4441f2f6
Sergey Bochkov <sergebochkov28@gmail.com>
2022-09-26 17:16:31 +0300

    Lab 3

--- src/main/java/Container/Container.java
+++ src/main/java/Container/Container.java
@@ -1,88 +1,64 @@
 package Container;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
+import java.util.List;
 
 /**
  * @author Sergey Bochkov
- * {@link <a href="https://ru.wikipedia.org/wiki/">...</a>Обратная_польская_запись}
- * @version Lab 2
+ * @version Lab 3
  */
 public class Container {
-    /**
-     * @param c символ из строки
-     * @return если пробел идем дальше
-     */
-    static boolean isDelim(char c) {
-        return c == ' ';
-    }
+    LinkedList<Integer> linkedList = new LinkedList<>();
+    List<Integer> list = new ArrayList<>();
 
-    /**
-     * @param c символ из строки
-     * @return если ариф. действие, то идем дальше
-     */
-    static boolean isOperator(char c) {
-        return c == '+' || c == '-' || c == '*' || c == '/' || c == '%';
-    }
-
-    /**
-     * @return приоритет операций
-     */
-    static int priority(char op) {
-        return switch (op) {
-            case '+', '-' -> 1;
-            case '*', '/', '%' -> 2;
-            default -> -1;
-        };
-    }
-
-    /**
-     * @param st лист с числами
-     * @param op символ из строки
-     *           Прикрепил ссылку на википедию в самом начале
-     */
-    static void processOperator(LinkedList<Integer> st, char op) {
-        int r = st.removeLast();
-        int l = st.removeLast();
-        switch (op) {
-            case '+' -> st.add(l + r);
-            case '-' -> st.add(l - r);
-            case '*' -> st.add(l * r);
-            case '/' -> st.add(l / r);
-            case '%' -> st.add(l % r);
+    public void testLinkedList() {
+        long start_0 = System.currentTimeMillis();
+        for (int i = 0; i < 5000000; i++) {
+            linkedList.add(i);
         }
-    }
+        System.out.println("Время работы операции add для LinkedList (в милисекундах) = " + (System.currentTimeMillis() - start_0));
 
-    /**
-     * @param s строка/выражение
-     * @return результат
-     */
-    public static int eval(String s) {
-        LinkedList<Integer> st = new LinkedList<Integer>();
-        LinkedList<Character> op = new LinkedList<Character>();
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (isDelim(c))
-                continue;
-            if (c == '(')
-                op.add('(');
-            else if (c == ')') {
-                while (op.getLast() != '(')
-                    processOperator(st, op.removeLast());
-                op.removeLast();
-            } else if (isOperator(c)) {
-                while (!op.isEmpty() && priority(op.getLast()) >= priority(c))
-                    processOperator(st, op.removeLast());
-                op.add(c);
-            } else {
-                StringBuilder operand = new StringBuilder();
-                while (i < s.length() && Character.isDigit(s.charAt(i)))
-                    operand.append(s.charAt(i++));
-                --i;
-                st.add(Integer.parseInt(operand.toString()));
-            }
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 100; i++) {
+            linkedList.add(1235, Integer.MAX_VALUE);
         }
-        while (!op.isEmpty())
-            processOperator(st, op.removeLast());
-        return st.get(0);
+        System.out.println("Время работы операции add(в середину) для LinkedList (в милисекундах) = " + (System.currentTimeMillis() - start));
+        long start_1 = System.currentTimeMillis();
+        for (int i = 0; i < 10000; i++) {
+            linkedList.get(i);
+        }
+        System.out.println("Время работы операции get для LinkedList (в милисекундах) = " + (System.currentTimeMillis() - start_1));
+        long start_2 = System.currentTimeMillis();
+        for (int i = 5000000; i >= 1; i--) {
+            linkedList.remove(i);
+        }
+        System.out.println("Время работы операции remove для List (в милисекундах) = " + (System.currentTimeMillis() - start_2));
     }
 
+    public void testArrayList() {
+        long start_0 = System.currentTimeMillis();
+        for (int i = 0; i < 5000000; i++) {
+            list.add(i);
+        }
+        System.out.println("Время работы операции add для List (в милисекундах) = " + (System.currentTimeMillis() - start_0));
+
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 100; i++) {
+            list.add(1235, Integer.MAX_VALUE);
+        }
+        System.out.println("Время работы операции add(в середину) для List (в милисекундах) = " + (System.currentTimeMillis() - start));
+        long start_1 = System.currentTimeMillis();
+        for (int i = 0; i < 10000; i++) {
+            list.get(i);
+        }
+        System.out.println("Время работы операции get для List (в милисекундах) = " + (System.currentTimeMillis() - start_1));
+        long start_2 = System.currentTimeMillis();
+        for (int i = 5000000; i >= 1; i--) {
+            list.remove(i);
+        }
+        System.out.println("Время работы операции remove для List (в милисекундах) = " + (System.currentTimeMillis() - start_2));
+    }
 }
+
