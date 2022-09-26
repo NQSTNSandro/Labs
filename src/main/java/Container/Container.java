a560f4cd51d77c8cc3be52f29468dc40d080cab4
Sergey Bochkov <sergebochkov28@gmail.com>
2022-09-26 16:27:39 +0300

    Lab 2

--- src/main/java/Container/Container.java
+++ src/main/java/Container/Container.java
@@ -1,45 +1,88 @@
 package Container;
-
 import java.util.LinkedList;
 
 /**
  * @author Sergey Bochkov
- * @version lab 1
+ * {@link <a href="https://ru.wikipedia.org/wiki/">...</a>Обратная_польская_запись}
+ * @version Lab 2
  */
 public class Container {
     /**
-     * Создаю связный список
+     * @param c символ из строки
+     * @return если пробел идем дальше
      */
-    private LinkedList<Object> list = new LinkedList<>();
-
-
-    /**
-     * @param obj - лемент добавляемый в список
-     */
-    public void add(Object obj) {
-        list.add(obj);
+    static boolean isDelim(char c) {
+        return c == ' ';
     }
 
     /**
-     * @param index - Индекс элемента, который необходимо удалить
+     * @param c символ из строки
+     * @return если ариф. действие, то идем дальше
      */
-    public void remove(int index) {
-        list.remove(index);
+    static boolean isOperator(char c) {
+        return c == '+' || c == '-' || c == '*' || c == '/' || c == '%';
     }
 
     /**
-     * Вывод списка в консоль
+     * @return приоритет операций
      */
-    public void show() {
-        System.out.println(list);
+    static int priority(char op) {
+        return switch (op) {
+            case '+', '-' -> 1;
+            case '*', '/', '%' -> 2;
+            default -> -1;
+        };
     }
 
     /**
-     * @param index - Индекс извлекаемого элемента
-     * @return - элемент который необходимо извлечь
+     * @param st лист с числами
+     * @param op символ из строки
+     *           Прикрепил ссылку на википедию в самом начале
      */
-    public Object get(int index) {
-        return list.get(index);
+    static void processOperator(LinkedList<Integer> st, char op) {
+        int r = st.removeLast();
+        int l = st.removeLast();
+        switch (op) {
+            case '+' -> st.add(l + r);
+            case '-' -> st.add(l - r);
+            case '*' -> st.add(l * r);
+            case '/' -> st.add(l / r);
+            case '%' -> st.add(l % r);
+        }
+    }
+
+    /**
+     * @param s строка/выражение
+     * @return результат
+     */
+    public static int eval(String s) {
+        LinkedList<Integer> st = new LinkedList<Integer>();
+        LinkedList<Character> op = new LinkedList<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (isDelim(c))
+                continue;
+            if (c == '(')
+                op.add('(');
+            else if (c == ')') {
+                while (op.getLast() != '(')
+                    processOperator(st, op.removeLast());
+                op.removeLast();
+            } else if (isOperator(c)) {
+                while (!op.isEmpty() && priority(op.getLast()) >= priority(c))
+                    processOperator(st, op.removeLast());
+                op.add(c);
+            } else {
+                StringBuilder operand = new StringBuilder();
+                while (i < s.length() && Character.isDigit(s.charAt(i)))
+                    operand.append(s.charAt(i++));
+                --i;
+                st.add(Integer.parseInt(operand.toString()));
+            }
+        }
+        while (!op.isEmpty())
+            processOperator(st, op.removeLast());
+        return st.get(0);
     }
 
 }
--- src/main/java/com/example/demo/LabsApplication.java
+++ src/main/java/com/example/demo/LabsApplication.java
@@ -5,12 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.util.Assert;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 public class LabsApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(LabsApplication.class, args);
     }
-
-
 }
