bfe214f4e9f8458d7300cdb836592cb10feda8c3
Sergey Bochkov <sergebochkov28@gmail.com>
2022-09-26 17:13:37 +0300

    Lab 3

--- src/main/java/com/example/demo/LabsApplication.java
+++ src/main/java/com/example/demo/LabsApplication.java
@@ -3,14 +3,13 @@ package com.example.demo;
 import Container.Container;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.util.Assert;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class LabsApplication {
     public static void main(String[] args) {
         SpringApplication.run(LabsApplication.class, args);
+        Container container=new Container();
+        container.testLinkedList();
+        container.testArrayList();
     }
 }
